991dadd8a38e85b66a506b377943f639dd3b7366
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-10-08 21:58:28 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Varun Singh on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
